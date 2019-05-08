674e9732d1b695a9bad8a6a4ebd511f317e58d95
Chun-Chi Wan <J.T.Wan@JTWan-2.local>
2019-05-08 12:31:20 +0800

    new app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
+// import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import posed, { PoseGroup } from 'react-pose';
+import Popup from "reactjs-popup";
+import * as Scroll from 'react-scroll';
+import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+// import scroll from "./scroll/scroll.js";
 
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={require('./images/pic1.png')}></img>
+        <div className="popup">
+          <Popup trigger={<button> Show detail</button>}>
+            <div><img id="mama" src={require('./images/popup.png')}></img></div>
+          </Popup>
+          {/* <scroll /> */}
+        </div>
     </div>
   );
 }
